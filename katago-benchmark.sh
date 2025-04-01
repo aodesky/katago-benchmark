9fe5da17c2880d290e8317cffe0d419f3789e485
Andrew O'Desky <andy.odesky@gmail.com>
2025-04-01 18:24:06 -0400

    absolute paths

--- katago-benchmark.sh
+++ katago-benchmark.sh
@@ -1,17 +1,17 @@
 cd /home/
 
 git clone https://github.com/lightvector/KataGo.git
-cd KataGo/cpp
+cd /home/KataGo/cpp
 
 #sudo apt install cuda-toolkit
 sudo apt-get install tensorrt libzip-dev openssl jq bc coreutils
 
 cmake . -DUSE_BACKEND=TENSORRT -DBUILD_DISTRIBUTED=
 make -j 4
-cd ..
+cd /home/KataGo
 
 curl -O https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b28c512nbt-s8476434688-d4668249792.bin.gz
 
-./cpp/katago benchmark -model kata1-b28c512nbt-s8476434688-d4668249792.bin.gz -config default_gtp.cfg >> /home/benchmark.log
+./home/KataGo/cpp/katago benchmark -model kata1-b28c512nbt-s8476434688-d4668249792.bin.gz -config default_gtp.cfg >> /home/benchmark.log
 
 
