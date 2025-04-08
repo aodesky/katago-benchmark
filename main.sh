e2656dd0fa24c9fb4a80e6a86333f76a78377dc7
Andrew O'Desky <andy.odesky@gmail.com>
2025-04-08 17:55:41 -0400

    add status messages to main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+echo "Installing Katago..."
+
 ~/katago-benchmark/install-packages.sh
+
+echo "Done!"
+
+echo "Running initial benchmark..."
 
 # run initial benchmark
 ~/KataGo/cpp/katago benchmark -model ~/kata1-b28c512nbt-s8476434688-d4668249792.bin.gz -config ~/2gpus.cfg >> ~/katago-benchmark/results/benchmark.log
 
+echo "Done!"
+
+echo "Running game analysis..."
+
 # run game analysis
 ~/katago-benchmark/analyze.sh >> ~/katago-benchmark/results/game-analysis.log
+
+echo "Done!"
 
